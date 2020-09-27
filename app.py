afb4d9bea0524d7da233403ce07c0292e95ed1e1
anagol <golosnoy69@gmail.com>
2020-09-27 18:36:11 +0300

    New

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+import psycopg2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'anatolihalasny1969'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zfroyompsfsaol:7344da5ce14b9b93c2cbf71fce63aa5495c07df4aa630ace92cef9938a089516@ec2-54-217-236-206.eu-west-1.compute.amazonaws.com:5432/d8ifnmcve3nmpm'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 login_manager = LoginManager()
